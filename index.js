e7baa87840bbaebf5ec81e422ba7a8f8e5ec12f2
Banda Kranthi Kumar <162495729+BandaKranthi31@users.noreply.github.com>
2024-04-12 06:36:00 +0530

    FirstCommit

---
+++ index.js
@@ -0,0 +1,22 @@
+const mainmenu = document.querySelector('.mainmenu');
+const closemenu = document.querySelector('.closemenu');
+const openMenu = document.querySelector('.openmenu');
+const items = document.querySelectorAll('nav .mainmenu li a');
+
+
+openMenu.addEventListener('click', show);
+closemenu.addEventListener('click', close);
+
+items.forEach(item => {
+    item.addEventListener('click', function(){
+        close();
+    })
+})
+
+function show(){
+    mainmenu.style.display = 'flex';
+    mainmenu.style.top = '0';
+}
+function close(){
+    mainmenu.style.top = '-100%';
+}
